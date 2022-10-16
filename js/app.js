18709dfe15a4a4dda73ef5366884701d6e26b4ee
Normand <normandlaredo@gmail.com>
2022-10-16 12:11:07 +0100

    Adding responsive design

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ var body = document.getElementsByTagName("body")[0];
 // Color change Section
 window.onscroll = function () {
   var scroll = window.pageYOffset;
-
+  console.log(scroll);
   if (scroll < 700) {
     body.style.backgroundColor = "#17171f";
-  } else if (scroll >= 700 && scroll < 1600) {
+  } else if (scroll >= 700 && scroll < 1400) {
     body.style.backgroundColor = "#DC1C13";
-  } else if (scroll >= 1600 && scroll < 2700) {
+  } else if (scroll >= 1400 && scroll < 2300) {
     body.style.backgroundColor = "#FFCC25";
-  } else if (scroll >= 2700 && scroll < 3600) {
+  } else if (scroll >= 2300 && scroll < 3600) {
     body.style.backgroundColor = "#17171f";
   }
 };
@@ -33,4 +33,20 @@ function windowScroll() {
 window.addEventListener("scroll", (ev) => {
   ev.preventDefault();
   windowScroll();
+});
+
+const menu = document.querySelector(".menu-burguer");
+const navbar = document.querySelector("nav");
+const bodyBlur = document.querySelector(".body");
+
+menu.addEventListener("click", function () {
+  if (navbar.classList.contains("menu-close")) {
+    navbar.classList.remove("menu-close");
+    navbar.classList.add("menu-open");
+    bodyBlur.classList.add("body-blur");
+  } else {
+    navbar.classList.remove("menu-open");
+    navbar.classList.add("menu-close");
+    bodyBlur.classList.remove("body-blur");
+  }
 });
